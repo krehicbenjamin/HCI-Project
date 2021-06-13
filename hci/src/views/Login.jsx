46df9793b414604d2876e58970b8ae9deca1f208
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-14 01:09:36 +0200

    creating login screen

--- hci/src/views/Login.jsx
+++ hci/src/views/Login.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-
+import "../login.css"
+import image from "../Images/bhpress1.png"
+import {Link} from "react-router-dom"
 class Login extends Component {
     state = {  }
     render() { 
-        return ( <div></div> );
+        return ( 
+
+            <div className ="wrapper fadeInDown">
+                <div className="formContent">
+                    <div className="fadeIn first">
+                        <img src={image} id="icon" className="login_image" />
+                    </div>
+                    <form>
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login"/>
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"/>
+                        <input type="submit" className="fadeIn fourth" value="Log In"/>
+                    </form>
+                    <div id="formFooter">
+                    <Link className="underlineHover" to="/register">Don't have an account?</Link>
+                    </div>
+                </div>
+            </div>
+            
+        );
     }
 }
  
