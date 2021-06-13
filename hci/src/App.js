45b4f62158aa371acdf0eb369190695896de4a5c
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-13 23:55:45 +0200

    Add react-router-dom and routes

--- hci/src/App.js
+++ hci/src/App.js
@@ -1,122 +1,51 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar"
 import Footer from "./Components/Footer/footer"
 import Card from "./Components/Card/Card"
 import Sidecard from "./Components/Sidecard/Sidecard"
 import image from "./Images/pepsi.jpeg"
+import Home from "./views/Home"
+import Category from "./views/Category"
+import Article from "./views/Article"
+import Login from "./views/Login"
+import Register from "./views/Register"
 import './App.css'
 function App() {
   return (
-    <div className="App">
+    <Router>
+<div className="App">
       <Navbar />
       <div className="content">
-        <div className="articles">
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-        <Card 
-          title='Title'
-          imageURL='https://picsum.photos/200/300'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        />
-      </div>
-        <div className="sidecard">
-        <img src={image} className="Ad"/>
-        <Sidecard 
-          title="title"
-          imageURL="https://picsum.photos/200/300"
-          time="1d"
-        />
-        <Sidecard 
-          title="title"
-          imageURL="https://picsum.photos/200/300"
-          time="1d"
-          />
-          <Sidecard 
-          title="title"
-          imageURL="https://picsum.photos/200/300"
-          time="1d"
-          />
-          <Sidecard 
-          title="title"
-          imageURL="https://picsum.photos/200/300"
-          time="1d"
-          />
-          <Sidecard 
-          title="title"
-          imageURL="https://picsum.photos/200/300"
-          time="1d"
-          />
-      </div>
-      </div>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/category1'>
+              <Category />
+            </Route>
+            <Route path='/category2'>
+              <Category />
+            </Route>
+            <Route path='/category3'>
+              <Category />
+            </Route>
+            <Route path='/article'>
+              <Article />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/register'>
+              <Register />
+            </Route>
+          </Switch>
+        
+        </div>
       <Footer />
     </div>
+    </Router>
+    
   );
 }
 
---
+++ hci/src/views/Home.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+import Card from "../Components/Card/Card"
+import Sidecard from "../Components/Sidecard/Sidecard"
+import image from "../Images/pepsi.jpeg"
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+          <div className="content">
+          <div className="articles">
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+          <Card 
+            title='Title'
+            imageURL='https://picsum.photos/200/300'
+            body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+          />
+        </div>
+          <div className="sidecard">
+          <img src={image} className="Ad"/>
+          <Sidecard 
+            title="title"
+            imageURL="https://picsum.photos/200/300"
+            time="1d"
+          />
+          <Sidecard 
+            title="title"
+            imageURL="https://picsum.photos/200/300"
+            time="1d"
+            />
+            <Sidecard 
+            title="title"
+            imageURL="https://picsum.photos/200/300"
+            time="1d"
+            />
+            <Sidecard 
+            title="title"
+            imageURL="https://picsum.photos/200/300"
+            time="1d"
+            />
+            <Sidecard 
+            title="title"
+            imageURL="https://picsum.photos/200/300"
+            time="1d"
+            />
+        </div>
+        </div>
+         );
+    }
+}
+ 
+export default Home;
