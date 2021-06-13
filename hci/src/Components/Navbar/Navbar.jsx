55e60e74b6b6019222806736c80737d9656c259a
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-14 01:34:06 +0200

    updating logo

--- hci/src/Components/Navbar/Navbar.jsx
+++ hci/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import {menuItems} from "./MenuItems";
 import "./navbar.css";
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import {Button} from "../Button/Button";
+import image from "../../Images/bhpress1.png";
+
 class Navbar extends Component {
     state = {
         click: false
@@ -17,7 +19,7 @@ class Navbar extends Component {
         return ( 
         
             <nav className="navbar">
-                <h1 className="navbar-logo"><i class="fas fa-react"></i> BHPress </h1>
+                <img src={image} alt="logo" className="navbar-logo"/>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.click ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
