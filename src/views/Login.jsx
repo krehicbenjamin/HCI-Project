0baf2fe3ee43e508714528c124327b6e7634d712
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-14 23:00:31 +0200

    fixing login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import "../login.css"
 import image from "../Images/bhpress1.png"
 import {Link} from "react-router-dom"
@@ -15,7 +16,7 @@ class Login extends Component {
                     <form>
                         <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" required/>
                         <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" required/>
-                        <input type="submit" className="fadeIn fourth" value="Log In"/>
+                        <Link to="/"><input type="submit" className="fadeIn fourth" value="Log In"/></Link>
                     </form>
                     <div id="formFooter">
                     <Link className="underlineHover" to="/register">Don't have an account?</Link>
